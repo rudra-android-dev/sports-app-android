8ffd2cc532d5d8a1a81a4a5a8ce68de2266ab3b4
Rudra Pratap <rudrasikarwar.dev@gmail.com>
2026-04-21 12:25:47 +0530

    Day 5: Added loading and error state handling

--- app/src/main/java/com/example/sportsapp/viewmodel/MatchViewModel.kt
+++ app/src/main/java/com/example/sportsapp/viewmodel/MatchViewModel.kt
@@ -3,6 +3,9 @@ package com.example.sportsapp.viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.sportsapp.data.Match
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import com.example.sportsapp.network.RetrofitInstance
 
 class MatchViewModel : ViewModel() {
 
@@ -10,7 +13,34 @@ class MatchViewModel : ViewModel() {
 
     var selectedMatch = mutableStateOf<Match?>(null)
 
+    var isLoading = mutableStateOf(false)
+
+    var errorMessage = mutableStateOf<String?>(null)
+
     fun selectMatch(match: Match) {
         selectedMatch.value = match
+    }
+
+    fun fetchMatches() {
+        viewModelScope.launch {
+
+            isLoading.value = true
+            errorMessage.value = null
+
+            try {
+                val response = RetrofitInstance.api.getMatches()
+
+                if (response.isSuccessful) {
+                    matches.value = response.body()?.events ?: emptyList()
+                } else {
+                    errorMessage.value = "Failed: ${response.code()}"
+                }
+
+            } catch (e: Exception) {
+                errorMessage.value = "Network error"
+            }
+
+            isLoading.value = false
+        }
     }
 }
